fd57ed05eb7ec25e7137b346d530dc16e2f2da39
jlsears <jen.lauren.sears@gmail.com>
2015-05-15 21:16:18 -0500

    login view rendering

--- routes/users.js
+++ routes/users.js
@@ -26,11 +26,17 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 
+/* GET users listing/Handle request for login form */
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
 
 // Handle the registration form post
 app.post("/register", function (req, res) {
   console.log("hit post /register");
   var newUser = new UserModel(req.body);
+  console.log('Here is the new user', newUser)
 
   newUser.save(function (err, user) {
     if (err) {
@@ -42,38 +48,38 @@ app.post("/register", function (req, res) {
 });
 
 
-// Handle the login action
-// app.post("/login", function (req, res) {
+//Handle the login action
+app.post("/login", function (req, res) {
 
-//   console.log('Hi, this is Node handling the /user/login route');
+  console.log('Hi, this is Node handling the /user/login route');
 
-//   // Attempt to log the user is with provided credentials
-//   UserController.login(req.body.username, req.body.password)
+  // Attempt to log the user is with provided credentials
+  UserController.login(req.body.username, req.body.password)
 
-//     // After the database call is complete and successful,
-//     // the promise returns the user object
-//     .then(function (validUser) {
+    // After the database call is complete and successful,
+    // the promise returns the user object
+    .then(function (validUser) {
 
-//       console.log('Ok, now we are back in the route handling code and have found a user');
-//       console.log('validUser',validUser);
-//       console.log('Find any merch that is assigned to the user');
+      console.log('Ok, now we are back in the route handling code and have found a user');
+      console.log('validUser',validUser);
+      console.log('Find any merch that is assigned to the user');
 
-//       // Now find the movies that belong to the user
-//       getUserMerch(validUser._id)
-//         .then(function (merch) {
-//           // Render the merch list
-//           res.redirect("/merch/list");
-//         })
-//         .fail(function (err) {
-//           sendError(req, res, {errors: err.message}, "Failed")
-//         });
-//     })
+      // Now find the movies that belong to the user
+      getUserMerch(validUser._id)
+        .then(function (merch) {
+          // Render the merch list
+          res.redirect("/merch/list");
+        })
+        .fail(function (err) {
+          sendError(req, res, {errors: err.message}, "Failed")
+        });
+    })
 
-//     // After the database call is complete but failed
-//     .fail(function (err) {
-//       console.log('Failed looking up the user');
-//       sendError(req, res, {errors: err.message}, "Failed")
-//     })
-// });
+    // After the database call is complete but failed
+    .fail(function (err) {
+      console.log('Failed looking up the user');
+      sendError(req, res, {errors: err.message}, "Failed")
+    })
+});
 
 module.exports = app;
